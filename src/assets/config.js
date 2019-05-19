b69db183a155c854303f249a76cba20c7d0cdb06
lutfi <mlutfiibra@outlook.com>
2019-05-20 00:29:19 +0700

    Fix typo

--- src/assets/config.js
+++ src/assets/config.js
@@ -7,7 +7,7 @@ const config = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID"
+  appId: process.env.APP_ID
 };
 
 firebase.initializeApp(config);
