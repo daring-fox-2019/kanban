3aefa0357e5fd7abd5f2180893480b4255b4fd12
lutfi <mlutfiibra@outlook.com>
2019-05-19 23:55:03 +0700

    Implement Kanban

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <footer class="footer">
+            <div class="footer__content">
+            &copy; 2019.
+            Dirancang oleh Muhammad Lutfi, di Jakarta
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scope>
+.footer {
+    background-color: #fff;
+    border-top: solid 1px #ededed;
+    margin-top: 5em;
+}
+.footer__content {
+    text-align: center;
+    padding: 8px 20px;
+}
+</style>
